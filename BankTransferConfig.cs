d66a43086b4ea23f42032d22d7ef6294377c61d9
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 17:12:10 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace jurnalmod8_103022300065
+{
+    internal class BankTransferConfig
+    {
+        private const string filePath = @"bank_transfer_config.json";
+        public string lang { get; set; } = "en";
+        public BankTransferConfig bt;
+        
+        class transfer
+        {
+           
+      
+            public int threshold { get; set; } = 25000000;
+            public int low_fee { get; set; } = 6500;
+            public int high_fee { get; set; } = 15000;
+            
+
+        }
+        List <string> methods { get; set; } = new List<string>{ "RTO(real - time)”, “SKN”, “RTGS”, “BI FAST"};
+        class confirmation
+        {
+            public string en { get; set; } = "yes";
+            public string id { get; set; } = "ya";
+        }
+        public void ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            bt = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+            Console.WriteLine(bt);
+        }
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(bt, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+        public BankTransferConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                
+
+                WriteNewConfigFile();
+            }
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using jurnalmod8_103022300065;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+        string configPath = "bank_transfer_config.json";
+        Console.WriteLine($"Please insert the amount of money to transfer: ? ({ config.lang})";
+        int uang =Convert.ToInt32( Console.ReadLine());
+        if(  > config.threshold)
+        {
+            config.
+        }
+    }
+}
